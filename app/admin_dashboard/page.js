48dcac0800971d745c4855e15d5904d121b54e9d
PoshikaM <poshika.m@kalvium.community>
2025-07-25 23:01:20 +0530

    protected routes

--- app/admin_dashboard/page.js
+++ app/admin_dashboard/page.js
@@ -1,7 +1,25 @@
+// import DashboardPage from "@/components/pages/dashboard-page";
+
+// const AdminDashboardPage = () => {
+//   return <DashboardPage />;
+// };
+
+// export default AdminDashboardPage;
+
+
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import DashboardPage from "@/components/pages/dashboard-page";
+import { authOptions } from '../api/auth/[...nextauth]/route';
 
-const AdminDashboardPage = () => {
+export default async function AdminDashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || session.user?.role !== 'admin') {
+    redirect('/signin');
+  }
+
+  console.log(session.user); // Submission requirement
+
   return <DashboardPage />;
-};
-
-export default AdminDashboardPage;
+}
--- app/student-dashboard/page.js
+++ app/student-dashboard/page.js
@@ -1,3 +1,15 @@
+// "use client";
+// import StudentDashboardPage from "@/components/pages/student-dashboard";
+
+// const StudentDashboard = () => {
+//   return <StudentDashboardPage />;
+// };
+
+// export default StudentDashboard;
+
+// app/student-dashboard/page.js
+"use client";
+
 import StudentDashboardPage from "@/components/pages/student-dashboard";
 
 const StudentDashboard = () => {
